8ae558d01335f255b93badc28e37ce648863be3f
Luiz_Henrique <luizaureliano.tds1.senai@gmail.com>
2025-03-25 13:13:50 -0300

    Feat: create tab navigator

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
-//import Drawer from "./navigation/drawerNavigator";
+
 import Tab from "./navigation/tabNavigator";
 
 
 export default function App() {
     return (
         <NavigationContainer>
-            {/*<Drawer />*/}
             <Tab />
         </NavigationContainer>
     );
